adc8912aedcd724c6c104ee2ccd68c3a80f07e10
Bruno Schuster <bruno_schuster@hotmail.com>
2024-08-06 15:20:20 -0300

    templates

--- recipes/views.py
+++ recipes/views.py
@@ -7,7 +7,7 @@ def home(request):
     })
 
 def contato(request):
-    return HttpResponse('recipes/contato.html')
+    return render (request, 'recipes/contato.html')
 
 def sobre(request):
     return HttpResponse('sobre')
